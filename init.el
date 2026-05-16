9bc7d88e09211779b1709f7976bbae6b3c624adf
Ollie Branch <ollieevelon@gmail.com>
2026-05-16 17:04:21 -0400

    Only run straight bootstrapping when on a desktop platform.

--- init.el
+++ init.el
@@ -13,26 +13,27 @@
 
 ;; BOOTSTRAPPING
 ;; straight.el bootstrap function
-(defun bootstrap-straight ()
-       (progn
-	 (defvar bootstrap-version)
-	 (let ((bootstrap-file
-		(expand-file-name
-		 "straight/repos/straight.el/bootstrap.el"
-		 (or (bound-and-true-p straight-base-dir)
-		     user-emacs-directory)))
-	       (bootstrap-version 7))
-	   (unless (file-exists-p bootstrap-file)
-	     (with-current-buffer
-		 (url-retrieve-synchronously
-		  "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-		  'silent 'inhibit-cookies)
-	       (goto-char (point-max))
-	       (eval-print-last-sexp)))
-	   (load bootstrap-file nil 'nomessage))
-	 (straight-use-package 'use-package)))
-(bootstrap-straight)
-(straight-use-package 'org)
+(when (not (eq system-type 'android))
+  (defun bootstrap-straight ()
+    (progn
+      (defvar bootstrap-version)
+      (let ((bootstrap-file
+	     (expand-file-name
+	      "straight/repos/straight.el/bootstrap.el"
+	      (or (bound-and-true-p straight-base-dir)
+		  user-emacs-directory)))
+	    (bootstrap-version 7))
+	(unless (file-exists-p bootstrap-file)
+	  (with-current-buffer
+	      (url-retrieve-synchronously
+	       "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	       'silent 'inhibit-cookies)
+	    (goto-char (point-max))
+	    (eval-print-last-sexp)))
+	(load bootstrap-file nil 'nomessage))
+      (straight-use-package 'use-package)))
+  (bootstrap-straight)
+  (straight-use-package 'org))
 (setq evil-want-keybinding nil)
 
 ;; functions & macros
