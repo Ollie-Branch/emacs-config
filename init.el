3be5b236fe9a6ab29c6bbdd58a3ae73b2f32f374
Ollie Branch <ollieevelon@gmail.com>
2026-09-17 07:01:42 -0400

    Remove ef-themes, trying meow, return unbound in corfu.

--- init.el
+++ init.el
@@ -349,7 +349,15 @@ parent directory created."
 		 (display-buffer-in-tab)
 		 (tab-name . "Agenda")))
   (add-to-list 'display-buffer-alist
+	       '((derived-mode . org-mode)
+		 (display-buffer-in-tab)
+		 (tab-name . "Org")))
+  (add-to-list 'display-buffer-alist
 	       '("\\*Agenda Commands\\*"
+		 (display-buffer-in-side-window)
+		 (side . bottom)))
+  (add-to-list 'display-buffer-alist
+	       '("\\*Org Attach\\*"
 		 (display-buffer-in-side-window)
 		 (side . bottom)))
   :bind (("C-c a" . org-agenda)
@@ -508,28 +516,9 @@ parent directory created."
      (4 . (variable-pitch regular 1.2))
      (agenda-date . (1.3))
      (agenda-structure . (variable-pitch light 1.4))
-     (t . (1.0)))))
-
-(use-package-ensure! ef-themes
-  :init
-  ;; This makes the Modus commands listed below consider only the Ef
-  ;; themes.  For an alternative that includes Modus and all
-  ;; derivative themes (like Ef), enable the
-  ;; `modus-themes-include-derivatives-mode' instead.  The manual of
-  ;; the Ef themes has a section that explains all the possibilities:
-  ;;
-  ;; - Evaluate `(info "(ef-themes) Working with other Modus themes or taking over Modus")'
-  ;; - Visit <https://protesilaos.com/emacs/ef-themes#h:6585235a-5219-4f78-9dd5-6a64d87d1b6e>
-  (ef-themes-take-over-modus-themes-mode 1)
-  :custom
-  ;; All customization here.
-  (setq modus-themes-mixed-fonts t)
-  (setq modus-themes-italic-constructs t)
+     (t . (1.0))))
   :config
-  ;; Finally, load your theme of choice (or a random one with
-  ;; `modus-themes-load-random', `modus-themes-load-random-dark',
-  ;; `modus-themes-load-random-light').
-  (modus-themes-load-theme 'ef-owl))
+  (modus-themes-load-theme 'modus-vivendi))
 
 (use-package-desktop! modus-zenburn
   :straight (modus-zenburn
@@ -547,6 +536,93 @@ parent directory created."
   (auto-dark-themes '((modus-zenburn) (modus-zenburn-light)))
   :config
   (auto-dark-mode))
+
+(use-package-desktop! meow
+  :straight t
+  :custom
+  (meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-mode-state-list
+   '((conf-mode . normal) (fundamental-mode . normal)
+     (help-mode . motion) (prog-mode . normal)
+     (text-mode . normal)))
+  :config
+  (meow-leader-define-key
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore))
+  (meow-global-mode 1))
 
 (use-package-desktop! avy
   :straight t
@@ -732,6 +808,9 @@ parent directory created."
   :custom
   (corfu-auto t)
   (corfu-auto-delay 0.2)
+  :bind
+  (:map corfu-map
+	("RET" . nil))
   :init (global-corfu-mode))
 
 (use-package-ensure! rainbow-delimiters
